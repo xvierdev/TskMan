d3f3466f090de75dc420eed392df2093b84a22bf
Weley Xavier <wesley.xvier@gmail.com>
2025-01-14 20:58:51 -0300

    :bricks: chore: added task id to improve task deletion.

--- TskMan/Entities/ManTask.cs
+++ TskMan/Entities/ManTask.cs
@@ -4,6 +4,7 @@ namespace TskMan.Entities
     // Represents a user task.
     class ManTask
     {
+        public int Id { get; private set; }            // Task id
         public string Title { get; set; }              // Title os task
         public string Content { get; set; }            // Task content
         public DateTime CreationDateTime { get; private set; } // Creation time
@@ -12,8 +13,9 @@ namespace TskMan.Entities
         {
 
         }
-        public ManTask(string title, string content, DateTime creationDateTime)
+        public ManTask(int id, string title, string content, DateTime creationDateTime)
         {
+            Id = id;
             Title = title;
             Content = content;
             CreationDateTime = creationDateTime;
