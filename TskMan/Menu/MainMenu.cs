92857e0e3943e239a206e77348dd08a48df6bf31
Wesley Xavier <wesley.xvier@gmail.com>
2025-01-15 00:16:55 -0300

    :bug: fix: improve catch exception if invalid user input.

--- TskMan/Menu/MainMenu.cs
+++ TskMan/Menu/MainMenu.cs
@@ -6,13 +6,21 @@ namespace TskMan.Menu
     {
         public static MenuOptions Show()
         {
-            Console.WriteLine("0 : Create new task.");
-            Console.WriteLine("1 : Show all tasks.");
-            Console.WriteLine("2 : View details.");
-            Console.WriteLine("3 : Delete task.");
-            Console.WriteLine("4 : Exit.");
-            Console.Write("Choose your option: ");
-            return Enum.Parse<MenuOptions>(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("0 : Create new task.");
+                Console.WriteLine("1 : Show all tasks.");
+                Console.WriteLine("2 : View details.");
+                Console.WriteLine("3 : Delete task.");
+                Console.WriteLine("4 : Exit.");
+                Console.Write("Choose your option: ");
+                return Enum.Parse<MenuOptions>(Console.ReadLine());
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return MenuOptions.None;
+            }
         }
     }
 }
