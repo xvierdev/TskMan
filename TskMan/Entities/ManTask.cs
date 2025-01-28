243eb821634a1468f935f6ff74fc42adb0662b25
Wesley Xavier <wesley.xvier@gmail.com>
2025-01-28 18:48:10 -0300

    Merge branch 'main' of https://github.com/xvierdev/TskMan

--- TskMan/Entities/ManTask.cs
+++ TskMan/Entities/ManTask.cs
@@ -6,7 +6,6 @@ namespace TskMan.Entities
     // Represents a user task.
     class ManTask
     {
-        public int Id { get; private set; }            // Task id
         public string Title { get; set; }              // Title os task
         public string Content { get; set; }            // Task content
         public DateTime CreationDateTime { get; private set; } // Creation time
